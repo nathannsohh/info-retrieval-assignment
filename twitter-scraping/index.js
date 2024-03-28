90567f8b8f599ce83eef4c7d3e1916d68fbd7646
Nathan Soh <nathansoh@gmail.com>
2024-03-28 21:47:08 +0800

    update original scraping script

--- twitter-scraping/index.js
+++ twitter-scraping/index.js
@@ -2,10 +2,24 @@ import fs from 'fs'
 import Rettwit from 'rettiwt-api'
 import retry from 'async-retry';
 
-let NUM_OF_TWEETS = 5000
+import parse from 'csv-parser'
 
-const rettiwt = new Rettwit.Rettiwt({ apiKey: "a2R0PVpmVk1abEsyY0tWd2k2VFg0R1ZCalVzcmdtZEc0cEJxSlVYa2cxTHc7dHdpZD0idT0xNzYwNTE3ODQ5OTkzMzU1MjY0IjtjdDA9YWU3NDk3MjZmOGJmZDQ2YjhkYWZiOTM5OWQ0NmRkZDQ7YXV0aF90b2tlbj1jOGEwYjUwMTRhY2ViOWMyNDU3ZTEzNTU3MzNiNzBhNTIwY2IxZmY4Ow=="});
-let csvContent = "id,createdAt,userName,fullText,replyTo,lang,quoteCount,retweetCount,replyCount,retweetCount,likeCount\r\n";
+let NUM_OF_TWEETS = 1000
+let TWEEET_IDS = new Set([])
+let MIN_ID = null;
+
+// const rettiwt = new Rettwit.Rettiwt();
+
+// // // Logging in an getting the API_KEY
+// rettiwt.auth.login('sohnathan555@gmail.com', 'webscraper555', 'B@ndung123')
+// .then(apiKey => {
+//     // Use the API_KEY
+// 	console.log(apiKey)
+// })
+
+const rettiwt = new Rettwit.Rettiwt({ apiKey: "a2R0PXRIMHQwNklHeFJTUk5Rb2tnTVRXakNLd2lvYzM4V1FSa2ZvQnZmY3o7dHdpZD0idT0xNzczMTc0NzI0NjQ0MDE2MTI4IjtjdDA9ZjQyYmU5MWY2MDFjZDQwY2U4NGU1Mjc1OWVlMmMyZDM7YXV0aF90b2tlbj04MDZhMjI0MzQxYjA2YjdkODVjMjQ0M2E0YzIwOGFjZWQyZDViZDkxOw=="});
+// const rettiwt = new Rettwit.Rettiwt({ apiKey: "a2R0PUxKckpaSERWVEkyTjkzNDVpV2o4Um5tV2hGQzNHZWJpS0xvNjY5dlA7dHdpZD0idT0xNzczMTk2MDM3MDA1NzEzNDA4IjtjdDA9MjA2ODFiMzI5YjUxMmU1YTlhYjkzNjA1YzQxMGFhNTk7YXV0aF90b2tlbj1iZmU0ZDEzNTVmYmJmY2U4OWViOGI4Y2U5ZWZlMDM5MGU4YzBkZTJhOw=="});
+let csvContent = "id,createdAt,fullName,userName,profileImage,fullText,replyTo,lang,quoteCount,retweetCount,replyCount,retweetCount,likeCount\r\n";
 let data = null;
 
 const escapeCsvField = (field) => {
@@ -19,8 +33,10 @@ const fetchDataChunk = async () => {
   try {
     data = await retry(
       async () => {
+        // let rettiwt = new Rettwit.Rettiwt()
         return await rettiwt.tweet.search({
-          includeWords: ["elon", "musk"]
+          fromUsers: ['BBCWorld', 'ChannelNewsAsia'],
+          includeWords: ['elon', 'musk']
         }, 20, data?.next.value);
       },
       {
@@ -35,10 +51,15 @@ const fetchDataChunk = async () => {
 
     NUM_OF_TWEETS -= data.list.length;
     data.list.forEach((row) => {
+      TWEEET_IDS.add(row.id)
+      if (MIN_ID === null || Number(row.id) < Number(MIN_ID)) MIN_ID = row.id
+      console.log(MIN_ID)
       let dataRow = [
         row.id,
         row.createdAt,
+        escapeCsvField(row.tweetBy.fullName),
         escapeCsvField(row.tweetBy.userName),
+        escapeCsvField(row.tweetBy.profileImage),
         escapeCsvField(row.fullText),
         escapeCsvField(row.replyTo),
         row.lang,
@@ -51,9 +72,9 @@ const fetchDataChunk = async () => {
       csvContent += dataRow.join(',') + "\r\n";
     });
 
+    fs.writeFileSync('news_tweets.csv', csvContent, 'utf-8');
     if (!data.next || NUM_OF_TWEETS <= 0) {
       // If there is no more data or reached the desired number of tweets, write the CSV file
-      fs.writeFileSync('my_data.csv', csvContent, 'utf-8');
       console.log("CSV File created successfully");
     } else {
       // Continue fetching more data
@@ -75,5 +96,90 @@ const fetchDataChunk = async () => {
   }
 };
 
+const fetchRepliesOfTweets = async () => {
+  try {
+    data = await retry(
+      async () => {
+        return await rettiwt.tweet.search({
+          toUsers: ['ChannelNewsAsia'],
+          language: "en",
+          sinceId: MIN_ID
+        }, 20, data?.next.value);
+      },
+      {
+        retries: 3, // Number of retries
+        minTimeout: 480000, // Initial timeout in milliseconds
+        maxTimeout: 480000, // Maximum timeout in milliseconds
+        randomize: true, // Randomize the timeouts
+      }
+    );
+
+    console.log(data);
+
+    data.list.forEach((row) => {
+      if ((row.replyTo !== undefined && TWEEET_IDS.has(row.replyTo)) || (row.quoted !== undefined && TWEEET_IDS.has(row.quoted))) {
+        let dataRow = [
+          row.id,
+          row.createdAt,
+          escapeCsvField(row.tweetBy.fullName),
+          escapeCsvField(row.tweetBy.userName),
+          escapeCsvField(row.tweetBy.profileImage),
+          escapeCsvField(row.fullText),
+          escapeCsvField(row.replyTo),
+          row.lang,
+          row.quoteCount,
+          row.retweetCount,
+          row.replyCount,
+          row.retweetCount,
+          row.likeCount
+        ];
+        csvContent += dataRow.join(',') + "\r\n";
+      }
+    });
+
+    fs.writeFileSync('replies2.csv', csvContent, 'utf-8');
+    if (!data.next) {
+      // If there is no more data or reached the desired number of tweets, write the CSV file
+      console.log("CSV File created successfully");
+    } else {
+      // Continue fetching more data
+      await fetchRepliesOfTweets();
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+
+    // Check if it's a rate-limiting error
+    if (error.statusCode === 429) {
+      const waitTimeInMinutes = 15;
+      console.log(`Rate limit exceeded. Waiting for ${waitTimeInMinutes} minutes before retrying...`);
+
+      // Wait for 15 minutes before calling fetchDataChunk again
+      setTimeout(() => {
+        fetchDataChunk();
+      }, waitTimeInMinutes * 60 * 1000); // Convert minutes to milliseconds
+    }
+  }
+}
+
+const readCSVFile = () => {
+  fs.createReadStream('./news_tweets.csv')
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on("data", (row) => {
+      TWEEET_IDS.add(row.id)
+      if (MIN_ID === null || Number(row.id) < Number(MIN_ID)) MIN_ID = row.id
+    })
+    .on("error", (error) => {
+      console.log(error)
+    })
+    .on("end", () => {
+      console.log("done")
+    })
+}
+
 // Start fetching data
-fetchDataChunk();
+// Part 1
+// await fetchDataChunk();
+
+// Part 2
+readCSVFile()
+await fetchRepliesOfTweets()
