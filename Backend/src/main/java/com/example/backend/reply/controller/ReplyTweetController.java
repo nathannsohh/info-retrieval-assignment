f7ddb63f05442b9e44ab00ac879a092151149585
Alexlaaa <leealessandro.sg@gmail.com>
2024-04-05 17:47:53 +0800

    fix: Removed array representation for text_general in schema file

--- Backend/src/main/java/com/example/backend/reply/controller/ReplyTweetController.java
+++ Backend/src/main/java/com/example/backend/reply/controller/ReplyTweetController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tweets")
+@RequestMapping("/api/reply-tweets")
 public class ReplyTweetController {
 
   private final ReplyTweetService replyTweetService;
